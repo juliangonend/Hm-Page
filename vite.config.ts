21ab74c324ccc6395d35557dfe024bc5fbff9958
Julian Gonzalez Endzweig <gonzalezjulian501@gmail.com>
2025-03-06 12:34:43 -0300

    bb

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Hm-Page"
+  base: "/Hm-Page",
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    },
+  }
 })
