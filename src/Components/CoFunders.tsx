e1902b4176ec268e6ad675d83cad0b5d0fddcfed
Julian Gonzalez Endzweig <gonzalezjulian501@gmail.com>
2025-03-07 11:40:06 -0300

    change

--- src/Components/CoFunders.tsx
+++ src/Components/CoFunders.tsx
@@ -4,7 +4,7 @@ import { Card } from "./Card"
 export const CoFunders = () => {
   return (
    <>
-    <div style={{marginTop:'20px'}}>
+    <div style={{marginTop:'20px' , marginRight:'10px', marginLeft:'10px'}}>
         <h2 className='w-100 d-flex justify-content-center'>Co-Fundadores</h2>
          <div className='d-flex w-100 justify-content-center align-items-center flex-wrap'>
             <Card title='Jose Gonzalez'
